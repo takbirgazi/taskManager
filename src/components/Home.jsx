35e12a32cafa49bded5955253917af9f608c9f51
takbirgazi <takbirgazibd@gmail.com>
2024-11-15 12:07:49 +0600

    Update Task

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import Task from "./Task";
 
 const Home = () => {
     return (
-        <div className="bg-gray-900">
+        <div className="bg-gray-900 min-h-screen">
             <Task />
         </div>
     );
